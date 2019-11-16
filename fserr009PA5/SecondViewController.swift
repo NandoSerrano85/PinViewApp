726f9fe32b65525d308fbe345b057effa2483552
Fernando Serrano <fserr009@fiu.edu>
2019-11-16 09:42:55 -0500

    Initial Commit

---
+++ fserr009PA5/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  fserr009PA5
+//
+//  Created by Fernando Serrano on 11/16/19.
+//  Copyright © 2019 FIU. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
